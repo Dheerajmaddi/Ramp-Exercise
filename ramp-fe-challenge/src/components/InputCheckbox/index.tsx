9704e9a2b11c01665128773a0643cac93b1a8b5d
dheerajmaddi <96648866+Dheerajmaddi@users.noreply.github.com>
2024-04-18 23:01:09 +0000

    Bug 2 fixed

--- ramp-fe-challenge/src/components/InputCheckbox/index.tsx
+++ ramp-fe-challenge/src/components/InputCheckbox/index.tsx
@@ -1,13 +1,21 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import classNames from "classnames";
+import { useRef } from "react";
+import { InputCheckboxComponent } from "./types";
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled,
+  onChange,
+}) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`);
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
+      {/* Fixed Bug 2: toggle action working as expected now*/}
+      {/* Added htmlFor attribute to label tag as the input tag is not displayed on the screen */}
       <label
+        htmlFor={inputId}
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
@@ -19,8 +27,11 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         className="RampInputCheckbox--input"
         checked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
+        onChange={() => {
+          console.log("in onChange");
+          onChange(!checked);
+        }}
       />
     </div>
-  )
-}
+  );
+};
